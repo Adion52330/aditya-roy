57d7ed5bb8e6a3ec1c42dd3ef8e34a1886bf4738
Aditya <85357798+Adion52330@users.noreply.github.com>
2023-06-29 14:44:58 +0530

    minor change

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import { ToastContainer } from "react-toastify";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between lg:p-0 px-16 ">
+    <main className="flex min-h-screen flex-col items-center justify-between lg:p-0 px-12 ">
       <ToastContainer />
       <Head>
         <title>Aditya Roy - Carde</title>
